26b4690ce2ef73523f69ec8b08c57de18379ac07
Alex Cera <alexcera007@gmail.com>
2014-07-08 17:49:05 +0800

    code refactor
    -use prototype pattern instead of returning an object literal.\n-refactor the method which gets direction into a functional approach. less code duplication.

--- snake.js
+++ snake.js
@@ -1,446 +1,378 @@
 if (!window.requestAnimationFrame) {
-	window.requestAnimationFrame = (function() {
-		return window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame ||
-			window.oRequestAnimationFrame ||
-			window.msRequestAnimationFrame ||
-			function(callback) {
-				//gameloop fallbacks to 60fps
-				window.setTimeout(callback, 1000 / 60);
-			};
-	})();
-}
-
-
-
-var canvas,
-	context,
-	Game,
-	Snake,
-	Food,
-	Direction;
-
-
-
-canvas = document.createElement('canvas');
-canvas.width = 800;
-canvas.height = 600;
-context = canvas.getContext('2d');
-document.body.appendChild(canvas);
-
-
-
-Game = (function() {
-	var ready = false;
-
-	var blockSize = 20;
-
-	var mapFillStyle = '#0D0D0D';
-
-	var mapStrokeStyle = '#002608';
-
-	var score = 0;
-
-	function start() {
-		ready = true;
-	};
-
-	function stop() {
-		ready = false;
-	};
-
-	function getBlockSize() {
-		return blockSize;
-	};
-
-	function applyStyle(ctx, x, y) {
-		ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
-		ctx.lineWidth = .5;
-		ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
-	};
-
-	function isReady() {
-		return ready;
-	};
-
-	function clearMap() {
-		context.fillStyle = mapFillStyle;
-		context.strokeStyle = mapStrokeStyle;
-		for (var x = canvas.width - blockSize; x >= 0; x--) {
-			for (var y = canvas.height - blockSize; y >= 0; y--) {
-				applyStyle(context, x, y);
-			};
-		};
-	};
-
-	function showStartText() {
-		context.font = "bold 35px sans-serif";
-		context.fillStyle = '#FFFFFF';
-		context.fillText("Press space bar to start!", canvas.width / 4, canvas.height / 2);
-	};
-
-	function showScore() {
-		context.font = "bold 18px sans-serif";
-		context.fillStyle = 'rgba(255, 255, 255, 0.3)';
-		context.fillText("Score: " + score, canvas.width - 120, canvas.height - 20);	
-	}
-	return {
-		// initial FPS/Speed of the game
-		FPS: 50,
-
-		start: start,
-
-		stop: stop,
-
-		blockSize: getBlockSize,
-
-		isReady: isReady,
-
-		clearMap: clearMap,
-
-		showStartText: showStartText,
-
-		applyStyle: applyStyle,
-
-		showScore: showScore,
-
-		addScore : function() {
-			score++;
-		},
-
-		resetScore: function() {
-			score = 0;
-		}
-	};
-}());
-
-
-
-;
-Direction = (function() {
-	return {
-		// keycode mappings for the arrow keys
-		LEFT: 37,
-		UP: 38,
-		RIGHT: 39,
-		DOWN: 40,
-	};
-}());
-
-
-
-Food = (function() {
-
-	var foodFillStyle = '#FF6363';
-	var foodStrokeStyle = '#FFA8A8';
-
-	var x = null;
-	var y = null;
-
-	var consumed = true;
-
-	function generateRandomCoordinates() {
-		return {
-			x: Math.floor(Math.random() * (canvas.width / Game.blockSize())),
-			y: Math.floor(Math.random() * (canvas.height / Game.blockSize()))
-		};
-	};
-
-	function isValidCoordinates(x, y) {
-		var valid = true;
-		Snake.forEachSnakeSegment(function(segment) {
-			if (segment.x === x && segment.y === y) {
-				valid = false;
-				this.break;
-			}
-		});
-		return valid;
-	}
-
-	function generateValidCoordinates() {
-		var haventFoundAValidCoordinates = true;
-		var coordinates;
-		while (haventFoundAValidCoordinates) {
-			coordinates = generateRandomCoordinates();
-			if (isValidCoordinates(coordinates.x, coordinates.y)) {
-				haventFoundAValidCoordinates = false;
-				break;
-			}
-		}
-		x = coordinates.x;
-		y = coordinates.y;
-	};
-
-	function checkIfConsumed() {
-		Snake.forEachSnakeSegment(function(segment) {
-			var snakeX = segment.x;
-			var snakeY = segment.y;
-			if (x === snakeX && y === snakeY) {
-				consumed = true;
-				this.break;
-			}
-		});
-	};
-
-	function draw() {
-
-		checkIfConsumed();
-
-		if (consumed) {
-			Game.addScore();
-			generateValidCoordinates();
-			consumed = false;
-			Snake.grow();
-		}
-
-		context.fillStyle = foodFillStyle;
-		context.strokeStyle = foodStrokeStyle;
-
-		Game.applyStyle(context, x, y);
-	};
-
-	function init() {
-		if (consumed) {
-			generateValidCoordinates();
-			consumed = false;
-		}
-
-		context.fillStyle = foodFillStyle;
-		context.strokeStyle = foodStrokeStyle;
-
-		Game.applyStyle(context, x, y);
-	};
-
-
-	function consumed() {
-		return consumed;
-	};
-
-	return {
-		consumed: consumed,
-		draw: draw,
-		init: init
-	};
-}());
-
-
-
-;
-Snake = (function() {
-
-	//initial size of the snake
-	var initialSnakeSize = 5;
-
-	//a queue data struct for the body of the snake
-	var segments = [];
-
-	//RIGHT = initial direction of snake
-	var direction = Direction.RIGHT;
-
-	var newDirection = Direction.RIGHT;
-
-	var snakeFillStyle = '#FFFFFF';
-
-	var snakeStrokeStyle = '#D1D1D1';
-
-	var shouldGrow = false;
-
-	function head() {
-		return segments[0];
-	};
-
-	function forEachSnakeSegment(action) {
-		if (action && action.length == 1) {
-			for (var i = segments.length - 1; i >= 0; i--) {
-				action(segments[i]);
-			};
-		}
-	}
-
-	function fillSegment(segment) {
-		context.fillStyle = snakeFillStyle;
-		context.strokeStyle = snakeStrokeStyle;
-		Game.applyStyle(context, segment.x, segment.y);
-	};
-
-	function getNextHeadBasedOnDirection() {
-		var x = head().x;
-		var y = head().y;
-		switch (newDirection) {
-			case Direction.RIGHT:
-				if (direction !== Direction.LEFT) {
-					x++;
-					direction = newDirection;
-				}
-				break;
-			case Direction.LEFT:
-				if (direction != Direction.RIGHT) {
-					x--;
-					direction = newDirection;
-				}
-				break;
-			case Direction.UP:
-				if (direction !== Direction.DOWN) {
-					y--;
-					direction = newDirection;
-				}
-				break;
-			case Direction.DOWN:
-				if (direction !== Direction.UP) {
-					y++;
-					direction = newDirection;
-				}
-				break;
-			default:
-				break;
-		};
-		return {
-			x: x,
-			y: y
-		};
-	};
-
-	function setDirection(keyCode) {
-		switch (keyCode) {
-			case Direction.RIGHT:
-				if (direction !== Direction.LEFT)
-					newDirection = Direction.RIGHT;
-				break;
-			case Direction.LEFT:
-				if (direction != Direction.RIGHT)
-					newDirection = Direction.LEFT;
-				break;
-			case Direction.UP:
-				if (direction !== Direction.DOWN)
-					newDirection = Direction.UP;
-				break;
-			case Direction.DOWN:
-				if (direction !== Direction.UP)
-					newDirection = Direction.DOWN;
-				break;
-			default:
-				break;
-		};
-	};
-
-	function init() {
-		segments = [];
-		direction = Direction.RIGHT;
-		newDirection = Direction.RIGHT;
-		for (var i = initialSnakeSize - 1; i >= 0; i--) {
-			segments.push({
-				x: i,
-				y: 0
-			});
-		};
-	};
-
-	/*
-	 *	updates the values of snake such as direction, etc etc.
-	 * 	according to user-input
-	 */
-	function update() {
-		var nextHead = getNextHeadBasedOnDirection();
-		if (shouldGrow) {
-			segments.unshift(nextHead);
-			shouldGrow = false;
-		} else {
-			var tail = segments.pop();
-			tail.x = nextHead.x;
-			tail.y = nextHead.y;
-			segments.unshift(tail);
-		}
-
-	};
-
-	/*
-	 *	draws the snake using the updated values
-	 */
-	function draw() {
-		forEachSnakeSegment(fillSegment);
-	};
-
-	function collided() {
-		var nextHead = getNextHeadBasedOnDirection();
-		var collided = false;
-
-		//self collision
-		forEachSnakeSegment(function(segment) {
-			if (nextHead.x === segment.x && nextHead.y === segment.y) {
-				collided = true;
-				this.break;
-			}
-		});
-
-		// x-wall collision
-		if (nextHead.x * Game.blockSize() > canvas.width - Game.blockSize() || nextHead.x < 0)
-			collided = true;
-
-		//y-wall collision
-		if (nextHead.y * Game.blockSize() > canvas.height - Game.blockSize() || nextHead.y < 0)
-			collided = true;
-
-		return collided;
-	};
-
-	function getSegments() {
-		return segments;
-	}
-
-
-	function grow() {
-		shouldGrow = true;
-	};
-
-	return {
-		update: update,
-		draw: draw,
-		init: init,
-		setDirection: setDirection,
-		collided: collided,
-		segments: getSegments,
-		grow: grow,
-		forEachSnakeSegment: forEachSnakeSegment
-	};
-
-})();
-
+    window.requestAnimationFrame = (function() {
+        return window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(callback) {
+                //gameloop fallbacks to 60fps
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+};
+
+(function(w) {
+
+    var _ready = false,
+        _blockSize = 20,
+        _mapFillStyle = '#0D0D0D',
+        _mapStrokeStyle = '#002608',
+        _score = 0,
+
+        Game = w.Game = function() {
+            if (!this instanceof arguments.callee)
+                return new Game();
+        };
+
+    Game.prototype.FPS = 50;
+
+    Game.prototype.start = function() {
+        _ready = true;
+    };
+
+    Game.prototype.stop = function() {
+        _ready = false;
+    };
+
+    Game.prototype.blockSize = function() {
+        return _blockSize;
+    };
+
+    Game.prototype.isReady = function() {
+        return _ready;
+    };
+
+    Game.prototype.clearMap = function() {
+        context.fillStyle = _mapFillStyle;
+        context.strokeStyle = _mapStrokeStyle;
+        for (var x = canvas.width - _blockSize; x >= 0; x--) {
+            for (var y = canvas.height - _blockSize; y >= 0; y--) {
+                this.applyStyle(context, x, y);
+            };
+        };
+    };
+
+    Game.prototype.showStartText = function() {
+        context.font = "bold 35px sans-serif";
+        context.fillStyle = '#FFFFFF';
+        context.fillText("Press space bar to start!", canvas.width / 4, canvas.height / 2);
+    };
+
+    Game.prototype.showScore = function() {
+        context.font = "bold 18px sans-serif";
+        context.fillStyle = 'rgba(255, 255, 255, 0.3)';
+        context.fillText("Score: " + _score, canvas.width - 120, canvas.height - 20);
+    };
+
+    Game.prototype.applyStyle = function applyStyle(ctx, x, y) {
+        ctx.fillRect(x * _blockSize, y * _blockSize, _blockSize, _blockSize);
+        ctx.lineWidth = .5;
+        ctx.strokeRect(x * _blockSize, y * _blockSize, _blockSize, _blockSize);
+    };
+
+    Game.prototype.addScore = function() {
+        _score++;
+    };
+
+    Game.prototype.resetScore = function() {
+        _score = 0;
+    };
+
+    return Game;
+
+})(window);
+
+
+
+(function(w) {
+
+    var _foodFillStyle = '#FF6363',
+        _foodStrokeStyle = '#FFA8A8',
+        _x = null,
+        _y = null,
+        _consumed = true,
+
+        Food = w.Food = function() {
+            if (!this instanceof arguments.callee)
+                return new Food();
+        };
+
+    function generateRandomCoordinates() {
+        return {
+            x: Math.floor(Math.random() * (canvas.width / game.blockSize())),
+            y: Math.floor(Math.random() * (canvas.height / game.blockSize()))
+        };
+    };
+
+    function isValidCoordinates(x, y) {
+        var valid = true;
+        snake.forEachSegment(function(segment) {
+            if (segment.x === x && segment.y === y) {
+                valid = false;
+                this.break;
+            }
+        });
+        return valid;
+    };
+
+    function generateValidCoordinates() {
+        var coordinates;
+        while (true) {
+            coordinates = generateRandomCoordinates();
+            if (isValidCoordinates(coordinates.x, coordinates.y)) {
+                _x = coordinates.x;
+        		_y = coordinates.y;
+        		break;
+            }
+        }
+    };
+
+    function checkIfConsumed() {
+        snake.forEachSegment(function(segment) {
+            var snakeX = segment.x,
+                snakeY = segment.y;
+            if (_x === snakeX && _y === snakeY) {
+                _consumed = true;
+                this.break;
+            }
+        });
+    };
+
+    Food.prototype.consumed = function() {
+        return _consumed;
+    };
+
+    Food.prototype.draw = function() {
+
+        checkIfConsumed();
+
+        if (_consumed) {
+            game.addScore();
+            generateValidCoordinates();
+            _consumed = false;
+            snake.grow();
+        }
+
+        context.fillStyle = _foodFillStyle;
+        context.strokeStyle = _foodStrokeStyle;
+
+        game.applyStyle(context, _x, _y);
+    };
+
+    Food.prototype.init = function() {
+        if (_consumed) {
+            generateValidCoordinates();
+            _consumed = false;
+        }
+
+        context.fillStyle = _foodFillStyle;
+        context.strokeStyle = _foodStrokeStyle;
+
+        game.applyStyle(context, _x, _y);
+    };
+
+    return Food;
+})(window);
+
+
+
+snake = (function(w) {
+
+    //initial size of the snake
+    var _initialsnakeSize = 5,
+        _segments = [],
+        _direction = 39,
+        _newDirection = 39,
+        _snakeFillStyle = '#FFFFFF',
+        _snakeStrokeStyle = '#D1D1D1',
+        _shouldGrow = false,
+
+        Snake = w.Snake = function() {
+            if (!this instanceof arguments.callee)
+                return new Snake();
+        };
+
+    function head() {
+        return _segments[0];
+    };
+
+    function fillSegment(segment) {
+        context.fillStyle = _snakeFillStyle;
+        context.strokeStyle = _snakeStrokeStyle;
+        game.applyStyle(context, segment.x, segment.y);
+    };
+
+    function forDirection(key, fn) {
+    	fn = fn || function() {};
+        switch (key) {
+            case 37:
+                if (_direction != 39)
+                    fn(key, -1, 0);
+                break;
+            case 38:
+                if (_direction !== 40)
+                    fn(key, 0, -1)
+                break;
+            case 39:
+                if (_direction !== 37)
+                    fn(key, 1, 0);
+                break;
+            case 40:
+                if (_direction !== 38)
+                    fn(key, 0, 1);
+                break;
+            default:
+                break;
+        };
+    };
+
+    function getNextHeadBasedOnDirection() {
+        var x = head().x,
+            y = head().y;
+        forDirection(_newDirection, function(direction, newX, newY) {
+            _direction = direction;
+            x += newX;
+            y += newY;
+        });
+        return {
+            x: x,
+            y: y
+        };
+    };
+
+    Snake.prototype.collided = function() {
+        var nextHead = getNextHeadBasedOnDirection(),
+            collided = false;
+
+        //self collision
+        this.forEachSegment(function(segment) {
+            if (nextHead.x === segment.x && nextHead.y === segment.y) {
+                collided = true;
+                this.break;
+            }
+        });
+
+        // x-wall collision
+        if (nextHead.x * game.blockSize() > canvas.width - game.blockSize() || nextHead.x < 0)
+            collided = true;
+
+        //y-wall collision
+        if (nextHead.y * game.blockSize() > canvas.height - game.blockSize() || nextHead.y < 0)
+            collided = true;
+
+        return collided;
+    };
+
+    /*
+     *	draws the snake using the updated values
+     */
+    Snake.prototype.draw = function() {
+        this.forEachSegment(fillSegment);
+    };
+
+    Snake.prototype.forEachSegment = function(fn) {
+        if (fn && fn.length == 1) {
+            for (var i = _segments.length - 1; i >= 0; i--) {
+                fn(_segments[i]);
+            };
+        }
+    };
+
+    Snake.prototype.init = function() {
+        _segments = [];
+        _direction = 39;
+        _newDirection = 39;
+        for (var i = _initialsnakeSize - 1; i >= 0; i--) {
+            _segments.push({
+                x: i,
+                y: 0
+            });
+        };
+    };
+
+    Snake.prototype.grow = function() {
+        _shouldGrow = true;
+    };
+
+    Snake.prototype.segments = function() {
+        return _segments;
+    };
+
+    Snake.prototype.setDirection = function(keyCode) {
+        forDirection(keyCode, function(direction) {
+            _newDirection = direction;
+        });
+    };
+
+    /*
+     *	updates the values of snake such as direction, etc etc.
+     * 	according to user-input
+     */
+    Snake.prototype.update = function() {
+        var nextHead = getNextHeadBasedOnDirection();
+        if (_shouldGrow) {
+            _segments.unshift(nextHead);
+            _shouldGrow = false;
+        } else {
+            var tail = _segments.pop();
+            tail.x = nextHead.x;
+            tail.y = nextHead.y;
+            _segments.unshift(tail);
+        }
+
+    };
+
+    return Snake;
+
+})(window);
+
+
+var canvas = (function() {
+        var c = document.createElement('canvas');
+        c.width = 800;
+        c.height = 600;
+        document.body.appendChild(c);
+        return c;
+    })(),
+    context = canvas.getContext('2d'),
+    game = new Game(),
+    snake = new Snake(),
+    food = new Food();
 
 
 var registerKeyListeners = function() {
-	document.addEventListener('keydown', function(e) {
-		if (32 === e.keyCode) {
-			Game.start();
-		}
-		Snake.setDirection(e.keyCode);
-	}, false);
+    document.addEventListener('keydown', function(e) {
+        if (32 === e.keyCode)
+            game.start();
+        snake.setDirection(e.keyCode);
+    }, false);
 };
 
 
 
 var gameLoop = function() {
-	setTimeout(function() {
-		var requestId = requestAnimationFrame(gameLoop);
-		if (Game.isReady()) {
-			Game.clearMap();
-			if (Snake.collided()) {
-				Game.stop();
-				Game.resetScore();
-			} else {
-				Snake.update();
-				Snake.draw();
-				Food.draw();
-			}
-		} else {
-			Game.clearMap();
-			Snake.init();
-			Snake.draw();
-			Food.init();
-			Game.showStartText();
-		}
-		Game.showScore();
+    setTimeout(function() {
+        var requestId = requestAnimationFrame(gameLoop);
+        if (game.isReady()) {
+            game.clearMap();
+            if (snake.collided()) {
+                game.stop();
+                game.resetScore();
+            } else {
+                snake.update();
+                snake.draw();
+                food.draw();
+            }
+        } else {
+            game.clearMap();
+            snake.init();
+            snake.draw();
+            food.init();
+            game.showStartText();
+        }
+        game.showScore();
 
-	}, 1000 / Game.FPS);
+    }, 1000 / game.FPS);
 };
 
 registerKeyListeners();
